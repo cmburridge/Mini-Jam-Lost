34f1ce240ffacd70e8d23ee6f7b959778ce33a35
Cameron Burridge <cmburridge4022@gmail.com>
2023-02-18 08:39:42 -0700

    Added Animated timers and new lights

---
+++ Mini Jam Lost/Assets/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Image imageFront;
+
+    public FloatData timeRemaining;
+
+    public float time;
+
+    public UnityEvent Timer0;
+
+    public float amount;
+    void Start()
+    {
+        TimerStart();
+    }
+    
+    void TimerStart()
+    {
+        StartCoroutine(timerTime());
+    }
+
+    IEnumerator timerTime()
+    {
+        while (timeRemaining.value > 0)
+        {
+            time = timeRemaining.value;
+            
+            yield return new WaitForSeconds(1);
+            timeRemaining.value -= amount * Time.deltaTime;
+            imageFront.fillAmount = timeRemaining.value / timeRemaining.OGValue;
+        }
+
+        if (timeRemaining.value < 0)
+        {
+            Timer0.Invoke();
+        }
+    }
+}
